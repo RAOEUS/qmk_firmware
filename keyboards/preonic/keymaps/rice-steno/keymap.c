6ad6c2f8fed609bbb70f7134278bb499e4479f57
Rice <30870769+RAOEUS@users.noreply.github.com>
2023-10-12 13:20:31 -0400

    Update keymap.c
    Move RES1 and RES2 keycodes in Steno layer to positions more suitable for the Stenotype Extended layout

--- keyboards/preonic/keymaps/rice-steno/keymap.c
+++ keyboards/preonic/keymaps/rice-steno/keymap.c
@@ -81,21 +81,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------.
      * |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |   #  |
      * |------+------+------+------+------+-------------+------+------+------+------+------|
-     * |  FN  |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
+     * | RES1 |   S  |   T  |   P  |   H  |   *  |   *  |   F  |   P  |   L  |   T  |   D  |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
-     * |      |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
+     * | RES2 |   S  |   K  |   W  |   R  |   *  |   *  |   R  |   B  |   G  |   S  |   Z  |
      * |------+------+------+------+------+------|------+------+------+------+------+------|
      * |      |      |      |      |      |      |      |      |      |      |      |      |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |EXTSTN|      |      |   #  |   A  |   O  |   E  |   U  |   #  |  PWR | RES1 | RES2 |
+     * |EXTSTN|      |      |   #  |   A  |   O  |   E  |   U  |   #  |  PWR |  FN  |      |
      * `-----------------------------------------------------------------------------------'
      */
     [_STENO] = LAYOUT_preonic_grid(
         STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC, 
-        STN_FN,  STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR, 
-        XXXXXXX, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR, 
+        STN_RE1, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR, 
+        STN_RE2, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR, 
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
-        EXT_STN, XXXXXXX, XXXXXXX, STN_N1,  STN_A,   STN_O,   STN_E,   STN_U,   STN_N2,  STN_PWR, STN_RE1, STN_RE2
+        EXT_STN, XXXXXXX, XXXXXXX, STN_N1,  STN_A,   STN_O,   STN_E,   STN_U,   STN_N2,  STN_PWR, STN_FN,  XXXXXXX
     ),
 
     /* Lower
